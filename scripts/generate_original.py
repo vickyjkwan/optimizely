65557296bf345da63f1c9b47d1a5fc2084d997ef
mathilda0902 <mathilda_sorciere@yahoo.com>
2019-01-24 13:17:37 -0800

    endpoint, extracting to functions, I think we are really close

--- scripts/generate_original.py
+++ scripts/generate_original.py
@@ -4,8 +4,23 @@ import popelines
 import os
 from datetime import datetime
 
+# a function to populate upper level key:val pairs into lower level list of dictionaries
+# returns a dictionary with the each row inherets all metadata from upper level
+def populating_vals(outer_dict, inner_flattened_list, destination_key):
+    # outer_dict is the upper level dict
+    # inner_flattened_list is the lower level list of dicts that are each flattened
+    # destination_key is the primary key of the lower level
+    updated_dict = {}
+    for k, v in outer_dict.items():
+        if k != destination_key:
+            updated_dict[k] = v
+        else:
+            updated_dict[k] = inner_flattened_list
+    return updated_dict
+
 
 # a function to detect deeply nested json file, with a mix of dictionaries and lists
+# return a dictionary with one layer, and all keys only contains '_' and no '-'
 def flatten(jayson, acc, prefix):
     if isinstance(jayson, dict):
         for k,v in jayson.items():
@@ -26,13 +41,27 @@ def flatten(jayson, acc, prefix):
     else:
         return acc
 
-def main():
- 
-############################################### Project-Experiment #############################################
+# a function that first duplicates outer level key:val pairs, then flattens the duped list
+# returns a list of flattened rows
+def flatten_dupe_vals(vals, key):
+    
+    duped_results = []
+    for element in vals[key]:
+        ts_dict = populating_vals(outer_dict=vals, inner_flattened_list=element, destination_key=key)
+        duped_results.append(ts_dict)
+
+    flattened_results = []
+    for element in duped_results:
+        flattened_results.append(flatten(element, {}, ''))
+        
+    return flattened_results
+
+
+def generate_project_experiment(project_endpoint, experiment_endpoint):
 
     # get all projects
     try:
-        response_proj = requests.get('https://api.optimizely.com/v2/projects', headers=headers)
+        response_proj = requests.get(project_endpoint, headers=headers)
         j_proj = json.loads(response_proj.text)
         response_proj.raise_for_status()
         print('Successfully read project data.')
@@ -54,7 +83,7 @@ def main():
         ) 
 
         try:
-            response_exp = requests.get('https://api.optimizely.com/v2/experiments', headers=headers, params=params)
+            response_exp = requests.get(experiment_endpoint, headers=headers, params=params)
             j_exp = json.loads(response_exp.text)
             response_exp.raise_for_status()
             print('Successfully read experiment data.')
@@ -72,14 +101,15 @@ def main():
         pope.write_to_bq(table_name='experiments', file_name='../uploads/experiments.json', append=True, ignore_unknown_values=False, bq_schema_autodetect=False)
         print(f"Successfully uploaded experiments for project {p_id}")
 
-        project_list.append(project)
+    project_list.append(project)
 
     pope.write_to_json(file_name='../uploads/projects.json', jayson=project_list, mode='w')
     pope.write_to_bq(table_name='projects', file_name='../uploads/projects.json', append=True, ignore_unknown_values=False, bq_schema_autodetect=False)
     print("Successfully uploaded all projects.")
 
-############################################### Experiment-Results #############################################
 
+
+def generate_experiment_results(ts_endpoint, ):
     # get result time series from all experiments:
     for exp in j_exp:
         experiment_id = exp['id']
@@ -105,63 +135,20 @@ def main():
         flattened_timeseries = []
         for element in new_j_ts['metrics'][0]['results'][0]['timeseries']:
             flattened_timeseries.append(flatten(element, {}, ''))
-        print(len(flattened_timeseries))
 
         # Replace old 'timeseries' with new 'flattened_timeseries'
-        updated_results = {}
-        for k, v in new_j_ts['metrics'][0]['results'][0].items():
-            if k == 'timeseries':
-                updated_results[k] = flattened_timeseries
-            else:
-                updated_results[k] = v
+        updated_results = populating_vals(outer_dict=new_j_ts['metrics'][0]['results'][0], inner_flattened_list=flattened_timeseries, destination_key='timeseries')
+        flattened_results = flatten_dupe_vals(vals=updated_results, key='timeseries')
 
-        duped_results = []
-        for timestamp in updated_results['timeseries']:
-            ts_dict = {}
-            for key, val in updated_results.items():
-                if key != 'timeseries':
-                    ts_dict[key] = val
-                else:
-                    ts_dict[key] = timestamp
-            duped_results.append(ts_dict)
+        # Replace old 'metrics' with new 'flattened_results'
+        update_metrics = populating_vals(outer_dict=new_j_ts['metrics'][0], inner_flattened_list=flattened_results, destination_key='results')
+        flattened_metrics = flatten_dupe_vals(vals=update_metrics, key='results')
 
-        flattened_results = []
-        for element in duped_results:
-            flattened_results.append(flatten(element, {}, ''))
-
-        ######### so far, each result timestamp is captured and flattened out in each row, with upper level metrics in the ['metrics'][0]['results'] populated to each row
-
-
-
-        # results_duped = []
-        # other_metric_keys = ['aggregator', 'event_id', 'name', 'scope', 'winning_direction']
-        # other_experiment_keys = ['confidence_threshold', 'end_time', 'experiment_id', 'start_time', 'stats_config']
-
-        # for metric in j_ts['metrics']:
-        #     for key, value in metric.items():
-        #         if key == 'results':
-        #             d = {}
-        #             for result_key, result_value in value.items():
-        #                 d[result_key] = flatten(result_value, {}, '')
-                        
-        #     for k, v in d.items():
-        #         new_d = {}
-        #         new_d['result_id'] = k
-        #         new_d['result'] = v
-        #         for key in other_metric_keys:
-        #             new_d[key] = metric[key]
-        #         for key in other_experiment_keys:
-        #             new_d[key] = j_ts[key]
-        #         results_duped.append(new_d)
-            
-        # new_result = []
-        # for result in results_duped:
-        #     new_result.append(flatten(result, {}, ''))
         
         # upload results 
-        pope.write_to_json(file_name='../uploads/result_ts.json', jayson=new_result, mode='w')
-        pope.write_to_bq(table_name='result_ts', file_name='../uploads/result_ts.json', append=True, ignore_unknown_values=False, bq_schema_autodetect=False)
-        print(f"Successfully uploaded result time series for all experiments in experiment {experiment_id}.")
+        pope.write_to_json(file_name='../uploads/result_ts.json', jayson=flattened_metrics, mode='w')
+        # pope.write_to_bq(table_name='result_ts', file_name='../uploads/result_ts.json', append=True, ignore_unknown_values=False, bq_schema_autodetect=False)
+        # print(f"Successfully uploaded result time series for all experiments in experiment {experiment_id}.")
 
 
 if __name__ == '__main__':
@@ -174,4 +161,11 @@ if __name__ == '__main__':
         'Authorization': 'Bearer 2:EWAWmaXb4TgtYVU2VvwoEF-9UbJxBahkiFh1633_Oc9nmju7iJis',
     }
 
-    main()
+    ############################################### Project-Experiment #############################################
+    p_endpoint = 'https://api.optimizely.com/v2/projects'
+    e_endpoint = 'https://api.optimizely.com/v2/experiments'
+
+    generate_project_experiment(project_endpoint=p_endpoint, experiment_endpoint=e_endpoint)
+
+
+    ############################################### Experiment-Results #############################################
