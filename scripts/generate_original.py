fb8b86cdd3706383a823af7e5705b08e2b15fdf3
mathilda0902 <mathilda_sorciere@yahoo.com>
2019-01-31 22:12:37 -0800

    generate_original is da thang

---
+++ scripts/generate_original.py
@@ -0,0 +1,136 @@
+import json
+import requests
+from main import flatten, fix_values
+import popelines
+import os
+
+
+# read endpoint, returns a json file of the HTTP request
+def read_endpoint(endpoint, headers_set, params_set=None):
+    try:
+        response = requests.get(endpoint, headers=headers_set, params=params_set)
+        response_text = json.loads(response.text)
+        response.raise_for_status()
+
+    except requests.exceptions.HTTPError as err:
+        print(err)
+
+    return response_text
+
+
+# generate all experiments given a project id
+def generate_experiments(project_id, experiment_endpoint, experiment_headers, experiment_params):
+    # get all experiments from one project
+    j_exp = read_endpoint(endpoint=experiment_endpoint, headers_set=experiment_headers, params_set=experiment_params)
+
+    # loop for all experiments in this project
+    experiment_id_list = []
+    upload_exp_list = []
+    for exp in j_exp:
+        exp['project_id'] = project_id
+        upload_exp_list.append(flatten(exp, {}, ''))
+        experiment_id_list.append(exp['id'])
+        
+    return upload_exp_list, experiment_id_list
+
+
+# generate all projects within account
+def generate_projects(project_endpoint, project_headers):
+    # get all projects
+    j_proj = read_endpoint(endpoint=project_endpoint, headers_set=project_headers)
+
+    return j_proj
+
+
+if __name__ == '__main__':
+
+    ############################################### Keys and Authentication #######################################
+    gbq_key = os.environ.get('GOOGLE_ACCOUNT_CREDENTIALS')
+    pope = popelines.popeline(dataset_id='optimizely', service_key_file_loc=gbq_key, directory='.', verbose=False)
+
+    # Optimizely parameters
+    headers = {
+        'Authorization': 'Bearer 2:EWAWmaXb4TgtYVU2VvwoEF-9UbJxBahkiFh1633_Oc9nmju7iJis',
+    }
+
+    # endpoints
+    project_endpoint = 'https://api.optimizely.com/v2/projects'
+    experiment_endpoint = 'https://api.optimizely.com/v2/experiments'
+    result_endpoint = f'https://api.optimizely.com/v2/experiments/{experiment_id}/timeseries'
+
+
+    ############################################### generate and upload all projects ##############################
+    all_projects = generate_projects(project_endpoint, headers)
+    # upload projects 
+    pope.write_to_json(file_name='../uploads/projects.json', jayson=all_projects, mode='w')
+    pope.write_to_bq(table_name='projects', file_name='../uploads/projects.json', append=True, ignore_unknown_values=False, bq_schema_autodetect=False)
+    
+
+    ############################################### generate and upload all experiments ###########################
+    # get a list of project_id from all_projects
+    project_id_list = []
+    for project in all_projects:
+        project_id_list.append(project['id'])
+
+    # to accumulate all experiment_id, for 
+    experiment_id_list = []
+
+    # loop over all project_id_list to get experiments within each project
+    for project_id in project_id_list:
+        # params include project_id (required) and experiments pulling per each request (default only 25)
+        params = (
+            ('project_id', project_id),
+            ('per_page', 100),
+        ) 
+
+        exp_list, exp_id_list = generate_experiments(project_id, experiment_endpoint, headers, params)
+        experiment_id_list.extend(exp_id_list)
+
+        # upload experiments
+        pope.write_to_json(file_name='../uploads/origin_experiments.json', jayson=exp_list, mode='w')
+        pope.write_to_bq(table_name='experiments', file_name='../uploads/origin_experiments.json', append=True, ignore_unknown_values=False, bq_schema_autodetect=False)
+        print(f"Successfully uploaded experiments for project {project_id}")
+
+    
+    ############################################### generate and upload all result time series ########################
+    # loop over all experiment_id in experiment_id_list from above
+    for experiment_id in experiment_id_list:
+        response_ts = requests.get(result_endpoint, headers=headers)
+
+        # if '' then the experiment has not started yet
+        if response_ts.text == '':
+            j_ts = {'experiment_id': experiment_id}
+        else:
+            j_ts = json.loads(response_ts.text)
+
+        new_j_ts = pope.fix_json_values(callback=fix_values, obj=j_ts, reset_key='results')
+
+        flattened_metrics = []
+        for metric in new_j_ts['metrics']:
+            for result in metric['results']:
+                for timeseries in result['timeseries']:
+                    new_dict = {}
+                    new_dict['aggregator'] = metric['aggregator']
+                    new_dict['event_id'] = metric['event_id']
+                    new_dict['name'] = metric['name']
+                    new_dict['scope'] = metric['scope']
+                    new_dict['winning_direction'] = metric['winning_direction']
+
+                    new_dict['result_is_baseline'] = result['is_baseline']
+                    new_dict['result_level'] = result['level']
+                    new_dict['result_name'] = result['name']
+                    new_dict['result_variation_id'] = result['variation_id']
+                    new_dict['result_id'] = result['results_id']
+                    
+                    new_dict['time_series_rate'] = timeseries['rate']
+                    new_dict['time_series_time'] = timeseries['time']
+                    new_dict['time_series_value'] = timeseries['value']
+                    new_dict['time_series_samples'] = timeseries['samples']
+                    new_dict['time_series_variance'] = timeseries['variance']
+
+                    flattened_metrics.append(new_dict)
+        new_j_ts['metrics'] = flattened_metrics
+
+        pope.write_to_json(file_name='../uploads/origin_results.json', jayson=[new_j_ts], mode='w')
+        pope.write_to_bq(table_name='results', file_name='../uploads/origin_results.json', append=True, ignore_unknown_values=False, bq_schema_autodetect=False)
+        print(f"Successfully uploaded result time series for experiment {experiment_id}")
--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,33 @@
 import requests
 import json
-import popelines
 import os
 from datetime import datetime
 
 
+# a function to detect deeply nested json file, with a mix of dictionaries and lists
+# return a dictionary with one layer, and all keys only contains '_' and no '-'
+def flatten(jayson, acc, prefix):
+    if isinstance(jayson, dict):
+        for k,v in jayson.items():
+            if prefix:
+                prefix_k = prefix + "_" + k
+            else: 
+                prefix_k = k
+            prefix_k = prefix_k.replace('-', '_')
+            
+            if isinstance(v, dict):
+                flatten(v, acc, prefix_k)
+            elif isinstance(v, list):
+                for j in v:
+                    flatten(j, acc, prefix_k)
+            else:
+                acc[prefix_k] = v
+        return acc 
+    else:
+        return acc
+
+
+########################### functions not in use #######################################
 def fix_values(value, key, reset_key):
     if key == reset_key:
         new_list = []
@@ -30,28 +53,6 @@ def populating_vals(outer_dict, inner_flattened_list, destination_key):
             updated_dict[k] = inner_flattened_list
     return updated_dict
 
-
-# a function to detect deeply nested json file, with a mix of dictionaries and lists
-# return a dictionary with one layer, and all keys only contains '_' and no '-'
-def flatten(jayson, acc, prefix):
-    if isinstance(jayson, dict):
-        for k,v in jayson.items():
-            if prefix:
-                prefix_k = prefix + "_" + k
-            else: 
-                prefix_k = k
-            prefix_k = prefix_k.replace('-', '_')
-            
-            if isinstance(v, dict):
-                flatten(v, acc, prefix_k)
-            elif isinstance(v, list):
-                for j in v:
-                    flatten(j, acc, prefix_k)
-            else:
-                acc[prefix_k] = v
-        return acc 
-    else:
-        return acc
 
 # a function that first duplicates outer level key:val pairs, then flattens the duped list
 # returns a list of flattened rows
