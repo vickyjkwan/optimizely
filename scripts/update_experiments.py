e8efb61d6e453d2edfa9852a4d900c86fab8cb1e
mathilda0902 <mathilda_sorciere@yahoo.com>
2019-03-04 11:19:11 -0800

    upload succeeded.

--- scripts/update_experiments.py
+++ scripts/update_experiments.py
@@ -8,12 +8,12 @@ from generate_original_with_timeseries import read_endpoint, generate_projects, 
 
 
 if __name__ == '__main__':
-    if not os.environ.get('GOOGLE_ACCOUNT_CREDENTIALS'):
-        os.environ['GOOGLE_ACCOUNT_CREDENTIALS'] = '/home/engineering/keyfile.json'
+    # if not os.environ.get('GOOGLE_ACCOUNT_CREDENTIALS'):
+    #     os.environ['GOOGLE_ACCOUNT_CREDENTIALS'] = '/home/engineering/keyfile.json'
     gbq_key = os.environ.get('GOOGLE_ACCOUNT_CREDENTIALS')
 
-    directory = str(os.path.abspath(os.path.dirname(__file__)))
-    # directory = os.getcwd()
+    # directory = str(os.path.abspath(os.path.dirname(__file__)))
+    directory = os.getcwd()
 
     pope = popelines.popeline(dataset_id='optimizely', service_key_file_loc=gbq_key, directory='.', verbose=False)
     headers = {
